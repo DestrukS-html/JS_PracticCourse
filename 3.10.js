8cd9eae43a02acc7ba85529ab2235be705943bfc
Ilya Gorshkov <XePaCeee@yandex.ru>
2025-06-03 15:48:59 +0300

    Create 3.10.js
    Тренажёр "Условное ветвление 2"

---
+++ 3.10.js
@@ -0,0 +1,9 @@
+let randomNumber = -20
+
+//Введите ваше решение ниже
+
+if (randomNumber >= 0){
+  console.log(`Число ${randomNumber} больше или равно нулю`)
+}else{
+  console.log(`Число ${randomNumber} меньше нуля`)
+}
