7026ec5d3980e1558fd7330a4ed4c14c46ae7887
Ilya Gorshkov <XePaCeee@yandex.ru>
2025-06-03 15:43:01 +0300

    Create 2.7.js
    Тренажёр "Инкременты 1"

---
+++ 2.7.js
@@ -0,0 +1,23 @@
+let counter = 0
+
+console.log("Список пользователей:");
+
+let user1 = "Аня"
+counter++
+console.log(counter + ")" + user1);
+
+let user2 = "Василий"
+counter++
+console.log(counter + ")" + user2);
+
+let user3 = "Ирина"
+counter+=1
+console.log(counter + ")" + user3);
+
+let user4 = "Женя"
+counter++
+console.log(counter + ")" + user4);
+
+let user5 = "Татьяна"
+counter++
+console.log(counter + ")" + user5);
