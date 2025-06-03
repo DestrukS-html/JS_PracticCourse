8b65873ba745b15d1e92439dc1223e60d5509bf5
Ilya Gorshkov <XePaCeee@yandex.ru>
2025-06-03 15:43:52 +0300

    Create 2.8.js
    Тренажёр "Инкременты 2"

---
+++ 2.8.js
@@ -0,0 +1,8 @@
+let count = 2 // Количество товара
+let price = 220 // Цена товара
+let percent = 15 // Скидка в процентах
+
+//Введите ваше решение ниже
+let first = (count * price) / 100 * percent
+let itog = count * price - first
+console.log(itog)
